a57722fc646b2b526bc3aff2c6aeddda772c6a0b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-07-10 08:48:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
 
-  return (
-    <>
+    return (
+        <>
 
-        <h1>hello world</h1>
-              </>
-  )
+            <h1>hello world</h1>
+        </>
+    )
 }
 
 export default App
